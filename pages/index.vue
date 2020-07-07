2afdddf36dd97da7138b0412a6cd6c476145b1a3
HamidNE <hamidne@mail.ru>
2020-07-08 00:45:43 +0430

    add skill component

--- pages/index.vue
+++ pages/index.vue
@@ -4,71 +4,7 @@
 
 		<banner />
 		<about />
-
-		<!--
-			=============================================
-				My SKill Section
-			==============================================
-			-->
-		<div class="section-portfo skill-section-portfo">
-			<div class="section-num hide-pr"><span>0</span><span>2</span></div>
-			<div class="container">
-				<h2 class="theme-title-two">Experties<span>.</span></h2>
-
-				<div class="row">
-					<div class="col-lg-6">
-						<div class="skill-bar">
-							<div class="progress">
-								<div class="progress-bar" data-percent="80">
-									<span class="percent-text"><span class="count"></span>%</span>
-								</div>
-							</div>
-							<div class="skill-title">Web Designing</div>
-							<span class="category">HTML/CSS</span>
-						</div>
-						<!-- /.skill-bar -->
-					</div>
-					<div class="col-lg-6">
-						<div class="skill-bar">
-							<div class="progress">
-								<div class="progress-bar" data-percent="63">
-									<span class="percent-text"><span class="count"></span>%</span>
-								</div>
-							</div>
-							<div class="skill-title">Graphic Design.</div>
-							<span class="category">UI/UX</span>
-						</div>
-						<!-- /.skill-bar -->
-					</div>
-					<div class="col-lg-6">
-						<div class="skill-bar">
-							<div class="progress">
-								<div class="progress-bar" data-percent="75">
-									<span class="percent-text"><span class="count"></span>%</span>
-								</div>
-							</div>
-							<div class="skill-title">Web Development</div>
-							<span class="category">WORDPRESS/PHP</span>
-						</div>
-						<!-- /.skill-bar -->
-					</div>
-					<div class="col-lg-6">
-						<div class="skill-bar">
-							<div class="progress">
-								<div class="progress-bar" data-percent="85">
-									<span class="percent-text"><span class="count"></span>%</span>
-								</div>
-							</div>
-							<div class="skill-title">Application Dev.</div>
-							<span class="category">ANDROID/ IOS</span>
-						</div>
-						<!-- /.skill-bar -->
-					</div>
-				</div>
-				<!-- /.row -->
-			</div>
-		</div>
-		<!-- /.skill-section-portfo -->
+		<skill />
 
 		<!--
 			=============================================
@@ -580,10 +516,11 @@
 <script>
 import PreLoader from '~/components/layouts/PreLoader.vue'
 import About from '~/components/pages/index/about/About.vue'
+import Skill from '~/components/pages/index/skill/Skill.vue'
 import Banner from '~/components/pages/index/banner/Banner.vue'
 
 export default {
-	components: { About, Banner, PreLoader },
+	components: { About, Skill, Banner, PreLoader },
 	head() {
 		return {
 			bodyAttrs: {
