a20299528eb2be50d8a19c75fba47c418a34907f
HamidNE <hamidne@mail.ru>
2020-07-08 01:02:53 +0430

    add footer layout component

---
+++ components/layouts/footer/Footer.vue
@@ -0,0 +1,82 @@
+<template>
+	<footer class="portfo-footer hide-pr">
+		<img src="images/shape/shape-7.svg" alt="" class="round-shape" />
+		<div class="container">
+			<div class="theme-title-one text-center hide-pr">
+				<div class="upper-title">Contact Us</div>
+				<h2 class="main-title">
+					Send me a message, I will be touch <br />with you shortly.
+				</h2>
+			</div>
+			<!-- /.theme-title-one -->
+
+			<form action="#">
+				<div class="row">
+					<div class="col-lg-6">
+						<div class="input-group">
+							<input type="text" required />
+							<label>Your Name</label>
+						</div>
+						<!-- /.input-group -->
+					</div>
+					<!-- /.col- -->
+					<div class="col-lg-6">
+						<div class="input-group">
+							<input type="email" required />
+							<label>Your Email Address</label>
+						</div>
+						<!-- /.input-group -->
+					</div>
+					<!-- /.col- -->
+					<div class="col-12">
+						<div class="input-group">
+							<input type="text" required />
+							<label>Your Subject</label>
+						</div>
+						<!-- /.input-group -->
+					</div>
+					<!-- /.col- -->
+					<div class="col-12">
+						<div class="input-group">
+							<textarea required></textarea>
+							<label>Your Message</label>
+						</div>
+						<!-- /.input-group -->
+					</div>
+					<!-- /.col- -->
+					<div class="col-12">
+						<div class="clearfix">
+							<button class="send-button">Send Message</button>
+						</div>
+					</div>
+				</div>
+				<!-- /.row -->
+			</form>
+
+			<div class="copyright-text">
+				<ul class="social-icon">
+					<li>
+						<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+					</li>
+					<li>
+						<a href="#"><i class="fa fa-github-alt" aria-hidden="true"></i></a>
+					</li>
+				</ul>
+				<p>&copy; Built with Love By CreativeGigs.</p>
+			</div>
+			<!-- /.copyright-text -->
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
