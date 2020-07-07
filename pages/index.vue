f566757052d3e39a0482dc4cd20de03c64fa7785
HamidNE <hamidne@mail.ru>
2020-07-07 23:41:55 +0430

    add about component and fix body classes

--- pages/index.vue
+++ pages/index.vue
@@ -3,92 +3,7 @@
 		<pre-loader v-show="false" />
 
 		<banner />
-
-		<!--
-			=============================================
-				About Me
-			==============================================
-			-->
-		<div class="about-me-portfo section-portfo">
-			<div class="container">
-				<div class="inner-content">
-					<h2 class="theme-title-two">About me<span>.</span></h2>
-					<div class="img-box hide-pr">
-						<img src="images/home/9.jpg" alt="" />
-					</div>
-					<div class="profile-tab">
-						<!-- Nav tabs -->
-						<ul class="nav nav-tabs hide-pr">
-							<li class="nav-item">
-								<a class="nav-link active" data-toggle="tab" href="#myself"
-									>Myself.</a
-								>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" data-toggle="tab" href="#education"
-									>Education.</a
-								>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" data-toggle="tab" href="#skill">Skill.</a>
-							</li>
-						</ul>
-
-						<!-- Tab panes -->
-						<div class="tab-content">
-							<div id="myself" class="tab-pane active">
-								<p>
-									Hello! I’m <span>Rashedul Kabir</span> a self-taught &
-									<span>award winning Digital Designer & Developer</span> with
-									over <span>five years work experience</span>. I started in my
-									children’s room, got pro at renowned digital agencies
-									<span>akij co</span> and nork <span>blue lebel</span>. Now I’m
-									based in Dhaka, Bangladesh, working for
-									<span>Apple</span> fredmansky and enjoying the life in the
-									countryside.
-								</p>
-							</div>
-							<div id="education" class="tab-pane fade">
-								<p>
-									<b>STATE UNIVERSITY</b>, State College, Pa.
-									<span>Bachelor of Arts in Advertising;</span> Minor: Spanish;
-									GPA: 3.35, anticipated in May 2014
-								</p>
-								<div class="tag">Notable Accomplishments:</div>
-								<ul>
-									<li>Dean's List: Fall 2011, Spring 2018</li>
-									<li>
-										Awarded Best Advertising Campaign Proposal for Health
-										Committee, State University, Spring 2013
-									</li>
-								</ul>
-							</div>
-							<div id="skill" class="tab-pane fade">
-								<p>
-									Sed ut perspiciatis
-									<span>unde omnis iste natus error sit</span> voluptatem
-									accusantium doloremque laudantium, totam rem aperiam.
-								</p>
-								<ul class="skill-list">
-									<li>Web Designing _<span>HTML/CSS</span></li>
-									<li>Graphic Design. _<span>UI/UX</span></li>
-									<li>Web Development. _<span>WORDPRESS/PHP</span></li>
-									<li>Application Dev. _<span>ANDROID/ IOS</span></li>
-								</ul>
-							</div>
-						</div>
-
-						<ul class="button-group">
-							<li><a href="#" class="download-button">Download CV.</a></li>
-							<li><a href="#" class="hire-button">Hire Me</a></li>
-						</ul>
-					</div>
-					<!-- /.profile-tab -->
-				</div>
-				<!-- /.inner-content -->
-			</div>
-		</div>
-		<!-- /.about-me-portfo -->
+		<about />
 
 		<!--
 			=============================================
@@ -664,8 +579,17 @@
 
 <script>
 import PreLoader from '~/components/layouts/PreLoader.vue'
+import About from '~/components/pages/index/about/About.vue'
 import Banner from '~/components/pages/index/banner/Banner.vue'
+
 export default {
-	components: { Banner, PreLoader },
+	components: { About, Banner, PreLoader },
+	head() {
+		return {
+			bodyAttrs: {
+				class: ['no-scroll-y', 'home-portfo'],
+			},
+		}
+	},
 }
 </script>
