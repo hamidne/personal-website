816a12027da110441bba75b23bbf59be697b1200
HamidNE <hamidne@mail.ru>
2020-07-08 00:59:00 +0430

    add testimonial section

--- pages/index.vue
+++ pages/index.vue
@@ -7,68 +7,7 @@
 		<skill />
 		<service />
 		<project />
-
-		<!--
-			=====================================================
-				Testimonial
-			=====================================================
-			-->
-		<div class="portfo-testimonial section-portfo">
-			<div class="section-num hide-pr"><span>0</span><span>5</span></div>
-			<div class="container clearfix">
-				<h2 class="theme-title-two">Testimonials<span>.</span></h2>
-
-				<div class="row">
-					<div class="col-lg-4">
-						<h2 class="inner-title">
-							What’s <br />
-							Our Client Say About Us.
-						</h2>
-					</div>
-
-					<div class="col-lg-8">
-						<div class="slider-wrapper">
-							<div class="agn-testimonial-slider">
-								<div class="item">
-									<p>
-										Having a home based business is a wonderful asset to your
-										life. The problem still stands, when it comes timeadvertise
-										your business for a cheap cost. I know you have looked for
-										to answer everywhere.
-									</p>
-									<div class="author-info clearfix">
-										<img src="images/home/2.jpg" alt="" class="author-img" />
-										<div class="name-info">
-											<h6 class="name">Rashed kabir</h6>
-											<span>Designer</span>
-										</div>
-									</div>
-								</div>
-								<div class="item">
-									<p>
-										Having a home based business is a wonderful asset to your
-										life. The problem still stands, when it comes timeadvertise
-										your business for a cheap cost. I know you have looked for
-										to answer everywhere.
-									</p>
-									<div class="author-info clearfix">
-										<img src="images/home/3.jpg" alt="" class="author-img" />
-										<div class="name-info">
-											<h6 class="name">Zubayer hasan</h6>
-											<span>Developer</span>
-										</div>
-									</div>
-								</div>
-							</div>
-							<!-- /.agn-testimonial-slider -->
-						</div>
-						<!-- /.slider-wrapper -->
-					</div>
-				</div>
-			</div>
-			<!-- /.container -->
-		</div>
-		<!-- /.portfo-testimonial -->
+		<testimonial />
 
 		<!--
 			=====================================================
@@ -203,9 +142,18 @@ import Skill from '~/components/pages/index/skill/Skill.vue'
 import Banner from '~/components/pages/index/banner/Banner.vue'
 import Project from '~/components/pages/index/project/Project.vue'
 import Service from '~/components/pages/index/service/Service.vue'
+import Testimonial from '~/components/pages/index/testimonial/Testimonial.vue'
 
 export default {
-	components: { About, Skill, Banner, Project, Service, PreLoader },
+	components: {
+		About,
+		Skill,
+		Banner,
+		Project,
+		Service,
+		PreLoader,
+		Testimonial,
+	},
 	head() {
 		return {
 			bodyAttrs: {
