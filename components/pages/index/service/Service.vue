d4f7f01be5faee5654db14036326bea758be5c43
HamidNE <hamidne@mail.ru>
2020-07-08 00:47:20 +0430

    add service section

---
+++ components/pages/index/service/Service.vue
@@ -0,0 +1,89 @@
+<template>
+	<div class="section-portfo our-service-portfo">
+		<div class="section-num hide-pr"><span>0</span><span>3</span></div>
+		<div class="container">
+			<div class="title-wrapper">
+				<h2 class="theme-title-two">Services<span>.</span></h2>
+				<p>
+					Lorem ipsum dolor sit amet, ut mea inciderint ullamcorper. Mei putant
+					deleniti
+				</p>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-6" data-aos="flip-right">
+					<div class="service-block">
+						<img src="images/icon/icon18.svg" alt="" class="icon" />
+						<h5 class="title">
+							<a href="#">Front-End & Responsive Coding.</a>
+						</h5>
+						<p>
+							Extreme attention to detail is the essence of et Boo’s unique
+							design concepts. In mals alienum scripserit sea, in alia aliquid
+							nec
+						</p>
+						<a href="#" class="read-more"><i class="flaticon-next"></i></a>
+					</div>
+					<!-- /.service-block -->
+				</div>
+				<!-- /.col- -->
+				<div class="col-lg-6">
+					<div class="service-block" data-aos="flip-right">
+						<img src="images/icon/icon19.svg" alt="" class="icon" />
+						<h5 class="title">
+							<a href="#">User Experience & Interface Design.</a>
+						</h5>
+						<p>
+							Extreme attention to detail is the essence of et Boo’s unique
+							design concepts. In mals alienum scripserit sea, in alia aliquid
+							nec
+						</p>
+						<a href="#" class="read-more"><i class="flaticon-next"></i></a>
+					</div>
+					<!-- /.service-block -->
+				</div>
+				<!-- /.col- -->
+				<div class="col-lg-6">
+					<div class="service-block" data-aos="flip-right">
+						<img src="images/icon/icon20.svg" alt="" class="icon" />
+						<h5 class="title">
+							<a href="#">Social Media & Online Marketing.</a>
+						</h5>
+						<p>
+							Extreme attention to detail is the essence of et Boo’s unique
+							design concepts. In mals alienum scripserit sea, in alia aliquid
+							nec
+						</p>
+						<a href="#" class="read-more"><i class="flaticon-next"></i></a>
+					</div>
+					<!-- /.service-block -->
+				</div>
+				<!-- /.col- -->
+				<div class="col-lg-6">
+					<div class="service-block" data-aos="flip-right">
+						<img src="images/icon/icon21.svg" alt="" class="icon" />
+						<h5 class="title">
+							<a href="#">Mobile Application & Game Development.</a>
+						</h5>
+						<p>
+							Extreme attention to detail is the essence of et Boo’s unique
+							design concepts. In mals alienum scripserit sea, in alia aliquid
+							nec
+						</p>
+						<a href="#" class="read-more"><i class="flaticon-next"></i></a>
+					</div>
+					<!-- /.service-block -->
+				</div>
+				<!-- /.col- -->
+			</div>
+			<!-- /.row -->
+		</div>
+		<!-- /.container -->
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,94 +5,7 @@
 		<banner />
 		<about />
 		<skill />
-
-		<!--
-			=============================================
-				Our Service
-			==============================================
-			-->
-		<div class="section-portfo our-service-portfo">
-			<div class="section-num hide-pr"><span>0</span><span>3</span></div>
-			<div class="container">
-				<div class="title-wrapper">
-					<h2 class="theme-title-two">Services<span>.</span></h2>
-					<p>
-						Lorem ipsum dolor sit amet, ut mea inciderint ullamcorper. Mei
-						putant deleniti
-					</p>
-				</div>
-
-				<div class="row">
-					<div class="col-lg-6" data-aos="flip-right">
-						<div class="service-block">
-							<img src="images/icon/icon18.svg" alt="" class="icon" />
-							<h5 class="title">
-								<a href="#">Front-End & Responsive Coding.</a>
-							</h5>
-							<p>
-								Extreme attention to detail is the essence of et Boo’s unique
-								design concepts. In mals alienum scripserit sea, in alia aliquid
-								nec
-							</p>
-							<a href="#" class="read-more"><i class="flaticon-next"></i></a>
-						</div>
-						<!-- /.service-block -->
-					</div>
-					<!-- /.col- -->
-					<div class="col-lg-6">
-						<div class="service-block" data-aos="flip-right">
-							<img src="images/icon/icon19.svg" alt="" class="icon" />
-							<h5 class="title">
-								<a href="#">User Experience & Interface Design.</a>
-							</h5>
-							<p>
-								Extreme attention to detail is the essence of et Boo’s unique
-								design concepts. In mals alienum scripserit sea, in alia aliquid
-								nec
-							</p>
-							<a href="#" class="read-more"><i class="flaticon-next"></i></a>
-						</div>
-						<!-- /.service-block -->
-					</div>
-					<!-- /.col- -->
-					<div class="col-lg-6">
-						<div class="service-block" data-aos="flip-right">
-							<img src="images/icon/icon20.svg" alt="" class="icon" />
-							<h5 class="title">
-								<a href="#">Social Media & Online Marketing.</a>
-							</h5>
-							<p>
-								Extreme attention to detail is the essence of et Boo’s unique
-								design concepts. In mals alienum scripserit sea, in alia aliquid
-								nec
-							</p>
-							<a href="#" class="read-more"><i class="flaticon-next"></i></a>
-						</div>
-						<!-- /.service-block -->
-					</div>
-					<!-- /.col- -->
-					<div class="col-lg-6">
-						<div class="service-block" data-aos="flip-right">
-							<img src="images/icon/icon21.svg" alt="" class="icon" />
-							<h5 class="title">
-								<a href="#">Mobile Application & Game Development.</a>
-							</h5>
-							<p>
-								Extreme attention to detail is the essence of et Boo’s unique
-								design concepts. In mals alienum scripserit sea, in alia aliquid
-								nec
-							</p>
-							<a href="#" class="read-more"><i class="flaticon-next"></i></a>
-						</div>
-						<!-- /.service-block -->
-					</div>
-					<!-- /.col- -->
-				</div>
-				<!-- /.row -->
-			</div>
-			<!-- /.container -->
-		</div>
-		<!-- /.our-service-portfo -->
+		<service />
 
 		<!--
 			=============================================
@@ -518,9 +431,10 @@ import PreLoader from '~/components/layouts/PreLoader.vue'
 import About from '~/components/pages/index/about/About.vue'
 import Skill from '~/components/pages/index/skill/Skill.vue'
 import Banner from '~/components/pages/index/banner/Banner.vue'
+import Service from '~/components/pages/index/service/Service.vue'
 
 export default {
-	components: { About, Skill, Banner, PreLoader },
+	components: { About, Skill, Banner, Service, PreLoader },
 	head() {
 		return {
 			bodyAttrs: {
