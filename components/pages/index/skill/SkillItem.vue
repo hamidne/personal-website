62ca4a1375a2f57887a569a9cc39071cfde2c0be
HamidNE <hamidne@mail.ru>
2020-07-08 20:13:39 +0430

    add skill item component

---
+++ components/pages/index/skill/SkillItem.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="skill-bar">
+		<div class="progress">
+			<div class="progress-bar" data-percent="80">
+				<span class="percent-text"><span class="count"></span>%</span>
+			</div>
+		</div>
+		<div class="skill-title">Web Designing</div>
+		<span class="category">HTML/CSS</span>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
