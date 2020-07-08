8e8aee6c0023cef7bc3810c89c8a63ad52821223
HamidNE <hamidne@mail.ru>
2020-07-08 20:26:06 +0430

    edit for skills section

--- components/pages/index/skill/Skill.vue
+++ components/pages/index/skill/Skill.vue
@@ -5,45 +5,13 @@
 			<h2 class="theme-title-two">Experties<span>.</span></h2>
 
 			<b-row>
-				<b-col lg="6">
-					<skill-item title="Web Designing" category="HTML/CSS" :percent="80" />
+				<b-col v-for="(item, index) in items" :key="index" lg="6">
+					<skill-item
+						:title="item.title"
+						:percent="item.percent"
+						:category="item.category"
+					/>
 				</b-col>
-				<div class="col-lg-6">
-					<div class="skill-bar">
-						<div class="progress">
-							<div class="progress-bar" data-percent="63">
-								<span class="percent-text"><span class="count"></span>%</span>
-							</div>
-						</div>
-						<div class="skill-title">Graphic Design.</div>
-						<span class="category">UI/UX</span>
-					</div>
-					<!-- /.skill-bar -->
-				</div>
-				<div class="col-lg-6">
-					<div class="skill-bar">
-						<div class="progress">
-							<div class="progress-bar" data-percent="75">
-								<span class="percent-text"><span class="count"></span>%</span>
-							</div>
-						</div>
-						<div class="skill-title">Web Development</div>
-						<span class="category">WORDPRESS/PHP</span>
-					</div>
-					<!-- /.skill-bar -->
-				</div>
-				<div class="col-lg-6">
-					<div class="skill-bar">
-						<div class="progress">
-							<div class="progress-bar" data-percent="85">
-								<span class="percent-text"><span class="count"></span>%</span>
-							</div>
-						</div>
-						<div class="skill-title">Application Dev.</div>
-						<span class="category">ANDROID/ IOS</span>
-					</div>
-					<!-- /.skill-bar -->
-				</div>
 			</b-row>
 		</b-container>
 	</div>
@@ -51,8 +19,17 @@
 
 <script>
 import SkillItem from '~/components/pages/index/skill/SkillItem.vue'
+
 export default {
 	components: { SkillItem },
+	data: () => ({
+		items: [
+			{ title: 'Web Designing', category: 'HTML/CSS', percent: 80 },
+			{ title: 'Graphic Design.', category: 'UI/UX', percent: 75 },
+			{ title: 'Web Development', category: 'WORDPRESS/PHP', percent: 75 },
+			{ title: 'Application Dev.', category: 'ANDROID/IOS', percent: 50 },
+		],
+	}),
 }
 </script>
 
