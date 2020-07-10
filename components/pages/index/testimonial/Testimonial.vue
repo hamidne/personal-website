74120a9cfd83cadf38f23144ba161a667fefa142
HamidNE <hamidne@mail.ru>
2020-07-10 11:16:29 +0430

    add TestimonialSlider component

--- components/pages/index/testimonial/Testimonial.vue
+++ components/pages/index/testimonial/Testimonial.vue
@@ -4,60 +4,27 @@
 		<div class="container clearfix">
 			<h2 class="theme-title-two">Testimonials<span>.</span></h2>
 
-			<div class="row">
-				<div class="col-lg-4">
+			<b-row class="row">
+				<b-col lg="4">
 					<h2 class="inner-title">
 						What’s <br />
 						Our Client Say About Us.
 					</h2>
-				</div>
+				</b-col>
 
-				<div class="col-lg-8">
-					<div class="slider-wrapper">
-						<div class="agn-testimonial-slider">
-							<div class="item">
-								<p>
-									Having a home based business is a wonderful asset to your
-									life. The problem still stands, when it comes timeadvertise
-									your business for a cheap cost. I know you have looked for to
-									answer everywhere.
-								</p>
-								<div class="author-info clearfix">
-									<img src="images/home/2.jpg" alt="" class="author-img" />
-									<div class="name-info">
-										<h6 class="name">Rashed kabir</h6>
-										<span>Designer</span>
-									</div>
-								</div>
-							</div>
-							<div class="item">
-								<p>
-									Having a home based business is a wonderful asset to your
-									life. The problem still stands, when it comes timeadvertise
-									your business for a cheap cost. I know you have looked for to
-									answer everywhere.
-								</p>
-								<div class="author-info clearfix">
-									<img src="images/home/3.jpg" alt="" class="author-img" />
-									<div class="name-info">
-										<h6 class="name">Zubayer hasan</h6>
-										<span>Developer</span>
-									</div>
-								</div>
-							</div>
-						</div>
-						<!-- /.agn-testimonial-slider -->
-					</div>
-					<!-- /.slider-wrapper -->
-				</div>
-			</div>
+				<b-col lg="8">
+					<testimonial-slider />
+				</b-col>
+			</b-row>
 		</div>
-		<!-- /.container -->
 	</div>
 </template>
 
 <script>
-export default {}
+import TestimonialSlider from '~/components/pages/index/testimonial/TestimonialSlider.vue'
+export default {
+	components: { TestimonialSlider },
+}
 </script>
 
 <style lang="scss" scoped></style>
---
+++ components/pages/index/testimonial/TestimonialSlider.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="slider-wrapper">
+		<div class="agn-testimonial-slider">
+			<div class="item">
+				<p>
+					Having a home based business is a wonderful asset to your life. The
+					problem still stands, when it comes timeadvertise your business for a
+					cheap cost. I know you have looked for to answer everywhere.
+				</p>
+				<div class="author-info clearfix">
+					<img src="images/home/2.jpg" alt="" class="author-img" />
+					<div class="name-info">
+						<h6 class="name">Rashed kabir</h6>
+						<span>Designer</span>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<p>
+					Having a home based business is a wonderful asset to your life. The
+					problem still stands, when it comes timeadvertise your business for a
+					cheap cost. I know you have looked for to answer everywhere.
+				</p>
+				<div class="author-info clearfix">
+					<img src="images/home/3.jpg" alt="" class="author-img" />
+					<div class="name-info">
+						<h6 class="name">Zubayer hasan</h6>
+						<span>Developer</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
