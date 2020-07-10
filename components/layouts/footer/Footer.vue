f35b1c21c270007ed2e24ea92964a518593a9ad2
HamidNE <hamidne@mail.ru>
2020-07-11 00:42:59 +0430

    add footer form components

--- components/layouts/footer/Footer.vue
+++ components/layouts/footer/Footer.vue
@@ -8,50 +8,8 @@
 					Send me a message, I will be touch <br />with you shortly.
 				</h2>
 			</div>
-			<!-- /.theme-title-one -->
 
-			<form action="#">
-				<div class="row">
-					<div class="col-lg-6">
-						<div class="input-group">
-							<input type="text" required />
-							<label>Your Name</label>
-						</div>
-						<!-- /.input-group -->
-					</div>
-					<!-- /.col- -->
-					<div class="col-lg-6">
-						<div class="input-group">
-							<input type="email" required />
-							<label>Your Email Address</label>
-						</div>
-						<!-- /.input-group -->
-					</div>
-					<!-- /.col- -->
-					<div class="col-12">
-						<div class="input-group">
-							<input type="text" required />
-							<label>Your Subject</label>
-						</div>
-						<!-- /.input-group -->
-					</div>
-					<!-- /.col- -->
-					<div class="col-12">
-						<div class="input-group">
-							<textarea required></textarea>
-							<label>Your Message</label>
-						</div>
-						<!-- /.input-group -->
-					</div>
-					<!-- /.col- -->
-					<div class="col-12">
-						<div class="clearfix">
-							<button class="send-button">Send Message</button>
-						</div>
-					</div>
-				</div>
-				<!-- /.row -->
-			</form>
+			<footer-form />
 
 			<div class="copyright-text">
 				<ul class="social-icon">
@@ -70,13 +28,16 @@
 				</ul>
 				<p>&copy; Built with Love By CreativeGigs.</p>
 			</div>
-			<!-- /.copyright-text -->
 		</div>
 	</footer>
 </template>
 
 <script>
-export default {}
+import FooterForm from '~/components/layouts/footer/FooterForm.vue'
+
+export default {
+	components: { FooterForm },
+}
 </script>
 
 <style lang="scss" scoped></style>
