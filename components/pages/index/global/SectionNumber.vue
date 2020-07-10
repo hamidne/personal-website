a8ba903c896de51e003eab079970e6367c2fb7a8
HamidNE <hamidne@mail.ru>
2020-07-11 00:18:02 +0430

    add section number component and use in slill section

---
+++ components/pages/index/global/SectionNumber.vue
@@ -0,0 +1,29 @@
+<template>
+	<div
+		v-observe-visibility="visibilityChanged"
+		class="section-num hide-pr"
+		:class="{ 'show-pr': visiable }"
+	>
+		<span>0</span>
+		<span v-text="number" />
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		number: {
+			type: Number,
+			default: 0,
+		},
+	},
+	data: () => ({ visiable: false }),
+	methods: {
+		visibilityChanged(visiable, entry) {
+			this.visiable = visiable
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped></style>
