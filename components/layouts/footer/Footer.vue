17db66b9d747028e89ee06622c56d3b86d39ac0e
HamidNE <hamidne@mail.ru>
2020-07-11 00:47:38 +0430

    fixed footer classes

--- components/layouts/footer/Footer.vue
+++ components/layouts/footer/Footer.vue
@@ -1,8 +1,15 @@
 <template>
-	<footer class="portfo-footer hide-pr">
+	<footer
+		v-observe-visibility="visibilityChanged"
+		class="portfo-footer hide-pr"
+		:class="{ 'show-pr': visiable }"
+	>
 		<img src="images/shape/shape-7.svg" alt="" class="round-shape" />
 		<div class="container">
-			<div class="theme-title-one text-center hide-pr">
+			<div
+				class="theme-title-one text-center hide-pr"
+				:class="{ 'show-pr': visiable }"
+			>
 				<div class="upper-title">Contact Us</div>
 				<h2 class="main-title">
 					Send me a message, I will be touch <br />with you shortly.
@@ -25,6 +32,12 @@ import FooterSocials from '~/components/layouts/footer/FooterSocials.vue'
 
 export default {
 	components: { FooterForm, FooterSocials },
+	data: () => ({ visiable: false }),
+	methods: {
+		visibilityChanged(visiable, entry) {
+			this.visiable = visiable
+		},
+	},
 }
 </script>
 
