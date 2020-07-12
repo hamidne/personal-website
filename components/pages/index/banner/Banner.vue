24c2e87dae3a8a30ef587413a2baa73cdbbdaf25
HamidNE <hamidne@mail.ru>
2020-07-12 07:40:04 +0430

    use from type string in banner section

--- components/pages/index/banner/Banner.vue
+++ components/pages/index/banner/Banner.vue
@@ -5,11 +5,17 @@
 				<h1 class="main-title wow fadeInUp animated" data-wow-delay="0.2s">
 					<span class="cd-headline clip">
 						<span>I Am Hamid Develope Quality</span>
-						<span class="cd-words-wrapper">
-							<b class="is-visible">Website<i>.</i></b>
-							<b>Mobile App<i>.</i></b>
-							<b>Landing Page<i>.</i></b>
-						</span>
+						<vue-typed-js
+							class="font-weight-semi-bold"
+							:strings="strings"
+							:loop="true"
+							:type-speed="40"
+							:back-speed="20"
+							:start-delay="1000"
+							content-type="html"
+						>
+							<span class="typing font-weight-normal text-decoration-none" />
+						</vue-typed-js>
 					</span>
 				</h1>
 			</div>
@@ -18,7 +24,11 @@
 </template>
 
 <script>
-export default {}
+export default {
+	data: () => ({
+		strings: ['Website<i>.</i>', 'Mobile App<i>.</i>', 'Landing Page<i>.</i>'],
+	}),
+}
 </script>
 
 <style lang="scss" scoped></style>
