84fefbda37d3fa47e7ca734f8d9f96c593516956
HamidNE <hamidne@mail.ru>
2020-07-12 22:21:30 +0430

    remove unused styles

--- configs/css.js
+++ configs/css.js
@@ -3,10 +3,6 @@ export default [
 	'~/assets/css/responsive.css',
 	'~/assets/css/custom-animation.css',
 	'~/assets/css/vendor/mega-menu/assets/css/menu.css',
-	'~/assets/css/vendor/fancybox/dist/jquery.fancybox.min.css',
-	'~/assets/css/vendor/sanzzy-map/dist/snazzy-info-window.min.css',
 	'~/assets/css/vendor/animated-headline-master/style.css',
-	'~/assets/css/vendor/iziModal-master/iziModal.css',
-	'~/assets/css/vendor/tabs/tabs.css',
 	'~/assets/scss/vendors/font-awesome/all.scss',
 ]
