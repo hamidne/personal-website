219c4ad30aef2043881660aefeec738605467dda
HamidNE <hamidne@mail.ru>
2020-07-13 19:03:31 +0430

    edit for header items

---
+++ components/layouts/header/HeaderItems.vue
@@ -0,0 +1,39 @@
+<template>
+	<nav id="mega-menu-holder" class="navbar navbar-expand-lg">
+		<div class="ml-auto nav-container">
+			<button
+				class="navbar-toggler navbar-toggler-right"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<i class="flaticon-setup" />
+			</button>
+			<div id="navbarSupportedContent" class="collapse navbar-collapse">
+				<ul class="navbar-nav">
+					<li
+						v-for="(item, index) in $t('layouts.header.items')"
+						:key="index"
+						class="nav-item dropdown"
+					>
+						<a
+							href="index.html"
+							class="nav-link dropdown-toggle"
+							data-toggle="dropdown"
+							v-text="item.title"
+						/>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- lang/en/layouts.js
+++ lang/en/layouts.js
@@ -1,5 +1,12 @@
 export default {
 	header: {
+		items: [
+			{ title: 'Home.', target: 'body' },
+			{ title: 'About.', target: 'body' },
+			{ title: 'Experties.', target: 'body' },
+			{ title: 'Services.', target: 'body' },
+			{ title: 'Projects.', target: 'body' },
+		],
 		socials: [
 			{ icon: 'fab fa-facebook-f', href: '' },
 			{ icon: 'fab fa-github', href: 'https://github.com/hamidne' },
