ce83a2decf258a0ee4839b85475f7ae78de2060f
HamidNE <hamidne@mail.ru>
2020-07-13 07:31:26 +0430

    edit for about tabs contents

--- components/pages/index/about/AboutTabs.vue
+++ components/pages/index/about/AboutTabs.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/no-v-html -->
 <template>
 	<div v-observe-visibility="visibilityChanged">
 		<b-tabs
@@ -5,44 +6,12 @@
 			content-class="mt-3"
 			:nav-class="{ 'show-pr': visiable }"
 		>
-			<b-tab title="Myself." active>
-				<p>
-					Hello! I’m <span>Rashedul Kabir</span> a self-taught &
-					<span>award winning Digital Designer & Developer</span> with over
-					<span>five years work experience</span>. I started in my children’s
-					room, got pro at renowned digital agencies <span>akij co</span> and
-					nork <span>blue lebel</span>. Now I’m based in Dhaka, Bangladesh,
-					working for <span>Apple</span> fredmansky and enjoying the life in the
-					countryside.
-				</p>
-			</b-tab>
-			<b-tab title="Education.">
-				<p>
-					<b>STATE UNIVERSITY</b>, State College, Pa.
-					<span>Bachelor of Arts in Advertising;</span> Minor: Spanish; GPA:
-					3.35, anticipated in May 2014
-				</p>
-				<div class="tag">Notable Accomplishments:</div>
-				<ul>
-					<li>Dean's List: Fall 2011, Spring 2018</li>
-					<li>
-						Awarded Best Advertising Campaign Proposal for Health Committee,
-						State University, Spring 2013
-					</li>
-				</ul>
-			</b-tab>
-			<b-tab title="Skill.">
-				<p>
-					Sed ut perspiciatis
-					<span>unde omnis iste natus error sit</span> voluptatem accusantium
-					doloremque laudantium, totam rem aperiam.
-				</p>
-				<ul class="skill-list">
-					<li>Web Designing _<span>HTML/CSS</span></li>
-					<li>Graphic Design. _<span>UI/UX</span></li>
-					<li>Web Development. _<span>WORDPRESS/PHP</span></li>
-					<li>Application Dev. _<span>ANDROID/ IOS</span></li>
-				</ul>
+			<b-tab
+				v-for="(item, index) in $t('pages.index.about.tabs')"
+				:key="index"
+				:title="item.title"
+			>
+				<p v-html="item.description" />
 			</b-tab>
 		</b-tabs>
 		<div class="profile-tab">
@@ -64,5 +33,3 @@ export default {
 	},
 }
 </script>
-
-<style lang="scss" scoped></style>
--- lang/en/pages.js
+++ lang/en/pages.js
@@ -10,6 +10,20 @@ export default {
 		},
 		about: {
 			title: 'About me',
+			tabs: [
+				{
+					title: 'Myself.',
+					description: `Hello! I’m Rashedul Kabir a self-taught & award winning Digital Designer & Developer with over five years work experience. I started in my children’s room, got pro at renowned digital agencies akij co and nork blue lebel. Now I’m based in Dhaka, Bangladesh, working for Apple fredmansky and enjoying the life in the countryside.`,
+				},
+				{
+					title: 'Education.',
+					description: `<p><b>STATE UNIVERSITY</b>, State College, Pa. <span>Bachelor of Arts in Advertising;</span> Minor: Spanish; GPA: 3.35, anticipated in May 2014</p><div class="tag">Notable Accomplishments:</div><ul><li>Dean's List: Fall 2011, Spring 2018</li><li>Awarded Best Advertising Campaign Proposal for Health Committee,	State University, Spring 2013	</li></ul>`,
+				},
+				{
+					title: 'Skill.',
+					description: `<p>Sed ut perspiciatis<span>unde omnis iste natus error sit</span> voluptatem accusantium	doloremque laudantium, totam rem aperiam.</p><ul class="skill-list"><li>Web Designing _<span>HTML/CSS</span></li><li>Graphic Design. _<span>UI/UX</span></li><li>Web Development. _<span>WORDPRESS/PHP</span></li><li>Application Dev. _<span>ANDROID/ IOS</span></li></ul>`,
+				},
+			],
 		},
 	},
 }
