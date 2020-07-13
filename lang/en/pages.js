16ba4884eca8b6d0aa569bcb47efdbc6c5eb3bb5
HamidNE <hamidne@mail.ru>
2020-07-13 07:16:30 +0430

    fixed index page contents

--- lang/en/pages.js
+++ lang/en/pages.js
@@ -1,1 +1,12 @@
-export default {}
+export default {
+	index: {
+		banner: {
+			title: 'I Am Hamid Develope Quality',
+			strings: [
+				'Website<i>.</i>',
+				'Mobile App<i>.</i>',
+				'Landing Page<i>.</i>',
+			],
+		},
+	},
+}
