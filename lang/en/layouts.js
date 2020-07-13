7017295b8e432accd2a45c8ced7274686eb8d77f
HamidNE <hamidne@mail.ru>
2020-07-13 19:48:32 +0430

    fixed navbar items scroll

--- lang/en/layouts.js
+++ lang/en/layouts.js
@@ -2,10 +2,10 @@ export default {
 	header: {
 		items: [
 			{ title: 'Home.', target: 'body' },
-			{ title: 'About.', target: 'body' },
-			{ title: 'Experties.', target: 'body' },
-			{ title: 'Services.', target: 'body' },
-			{ title: 'Projects.', target: 'body' },
+			{ title: 'About.', target: '.about-me-portfo' },
+			{ title: 'Experties.', target: '.skill-section-portfo' },
+			{ title: 'Services.', target: '.our-service-portfo' },
+			{ title: 'Projects.', target: '.our-project-portfo' },
 		],
 		socials: [
 			{ icon: 'fab fa-facebook-f', href: '' },
