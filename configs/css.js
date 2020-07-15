2d169571ec6d405ae730452947b62c0d75f43b50
HamidNE <hamidne@mail.ru>
2020-07-15 20:43:21 +0430

    comment mega-menu styles

--- configs/css.js
+++ configs/css.js
@@ -2,7 +2,7 @@ export default [
 	'~/assets/css/style.css',
 	'~/assets/css/responsive.css',
 	'~/assets/css/custom-animation.css',
-	'~/assets/css/vendor/mega-menu/assets/css/menu.css',
+	// '~/assets/css/vendor/mega-menu/assets/css/menu.css',
 	'~/assets/css/vendor/animated-headline-master/style.css',
 	'~/assets/scss/vendors/font-awesome/all.scss',
 ]
