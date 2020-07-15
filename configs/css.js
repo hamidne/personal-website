67a080d5d06db49c4e70a0f00869018477578318
HamidNE <hamidne@mail.ru>
2020-07-15 20:44:40 +0430

    comment other package

--- configs/css.js
+++ configs/css.js
@@ -3,6 +3,6 @@ export default [
 	'~/assets/css/responsive.css',
 	'~/assets/css/custom-animation.css',
 	// '~/assets/css/vendor/mega-menu/assets/css/menu.css',
-	'~/assets/css/vendor/animated-headline-master/style.css',
+	// '~/assets/css/vendor/animated-headline-master/style.css',
 	'~/assets/scss/vendors/font-awesome/all.scss',
 ]
