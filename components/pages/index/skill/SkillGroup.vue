41f8855c713c476691b268c27572b9d27fe16216
HamidNE <hamidne@mail.ru>
2020-07-27 00:41:44 +0430

    edit for skill section contents

--- components/pages/index/skill/SkillGroup.vue
+++ components/pages/index/skill/SkillGroup.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="skill-group">
-		<h2 class="skill-group__title" v-text="item.title" />
+		<h2 class="skill-group__title d-flex" v-text="item.title" />
 		<skill-items :items="item.items" />
 	</div>
 </template>
@@ -27,5 +27,16 @@ export default {
 }
 .skill-group__title {
 	color: #e1a34c;
+	white-space: nowrap;
+	&::after {
+		content: ' ';
+		width: 100%;
+		height: 1px;
+		margin: auto;
+		margin-left: 1rem;
+		display: inline-flex;
+		background: #e1a34c;
+		vertical-align: middle;
+	}
 }
 </style>
