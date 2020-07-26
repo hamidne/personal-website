dc768f6a211ef84b1617b435f065b7e200bde4b3
HamidNE <hamidne@mail.ru>
2020-07-27 00:00:58 +0430

    edit for skill section contents

--- components/pages/index/skill/Skill.vue
+++ components/pages/index/skill/Skill.vue
@@ -3,30 +3,22 @@
 		<section-number :number="2" />
 		<b-container>
 			<section-title :title="$t('pages.index.skill.title')" />
-			<b-row>
-				<b-col
-					v-for="(item, index) in $t('pages.index.skill.items')"
-					:key="index"
-					lg="6"
-				>
-					<skill-item
-						:title="item.title"
-						:percent="item.percent"
-						:category="item.category"
-					/>
-				</b-col>
-			</b-row>
+			<skill-group
+				v-for="(item, index) in $t('pages.index.skill.items')"
+				:key="index"
+				:item="item"
+			/>
 		</b-container>
 	</div>
 </template>
 
 <script>
-import SkillItem from '~/components/pages/index/skill/SkillItem.vue'
+import SkillGroup from '~/components/pages/index/skill/SkillGroup.vue'
 import SectionTitle from '~/components/pages/index/global/SectionTitle.vue'
 import SectionNumber from '~/components/pages/index/global/SectionNumber.vue'
 
 export default {
-	components: { SkillItem, SectionTitle, SectionNumber },
+	components: { SkillGroup, SectionTitle, SectionNumber },
 }
 </script>
 
