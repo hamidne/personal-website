a1305c856d8dfc3828209e504c418831ca01c8b0
HamidNE <hamidne@mail.ru>
2020-07-27 22:40:57 +0430

    edit for skills section

--- components/pages/index/skill/SkillItem.vue
+++ components/pages/index/skill/SkillItem.vue
@@ -1,7 +1,8 @@
 <template>
-	<div class="skill-bar py-3">
+	<div class="skill-bar position-relative py-5">
 		<div class="skill-title" v-text="item.title" />
-		<span class="category" v-text="item.subtitle" />
+		<span v-if="item.subtitle" class="category" v-text="item.subtitle" />
+		<div v-else class="skill-prog" :style="{ width: item.percent + '%' }" />
 	</div>
 </template>
 
@@ -17,4 +18,17 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.skill-prog {
+	width: 30%;
+	height: 1px;
+	background: #e1a34c80;
+	&:after {
+		content: ' ';
+		width: 100%;
+		height: 1px;
+		position: absolute;
+		background: #e1a34c14;
+	}
+}
+</style>
