b9e962be35c37c7b864946dca6589b93c2fe0daf
HamidNE <hamidne@mail.ru>
2021-03-28 00:31:11 +0430

    update: update configs

--- configs/css.js
+++ configs/css.js
@@ -1,7 +1,5 @@
 export default [
-	'~/assets/css/style.css',
-	'~/assets/css/responsive.css',
-	'~/assets/css/custom-animation.css',
+	'~/assets/scss/app.scss',
 	'~/assets/css/vendor/mega-menu/assets/css/menu.css',
 	'~/assets/css/vendor/animated-headline-master/style.css',
 	'~/assets/scss/vendors/font-awesome/all.scss',
--- configs/i18n.js
+++ configs/i18n.js
@@ -5,6 +5,11 @@ export default {
 			iso: 'en-US',
 			file: 'en/main.js',
 		},
+		{
+			code: 'fa',
+			iso: 'fa-IR',
+			file: 'fa/main.js',
+		},
 	],
 	seo: false,
 	lazy: true,
