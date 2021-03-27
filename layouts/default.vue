80838e6c126817cda75548da89d29d895112054c
HamidNE <hamidne@mail.ru>
2021-03-28 00:35:44 +0430

    feat: detect direction with lang

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,7 +15,13 @@ import ScrollToTop from '~/components/layouts/ScrollToTop.vue'
 export default {
 	components: { HeaderLayout, FooterLayout, ScrollToTop },
 	head() {
+		const i18nSeo = this.$nuxtI18nSeo()
 		return {
+			htmlAttrs: {
+				lang: this.$i18n.locale,
+				dir: ['ar', 'fa'].includes(this.$i18n.locale) ? 'rtl' : 'ltr',
+				...i18nSeo.htmlAttrs,
+			},
 			bodyAttrs: {
 				class: ['no-scroll-y', 'home-portfo'],
 			},
