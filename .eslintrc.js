705b65165830be5c713d4d77e4db370628755ace
HamidNE <hamidne@mail.ru>
2021-03-28 00:27:33 +0430

    lint: update eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,35 @@ module.exports = {
 	root: true,
 	env: {
 		browser: true,
-		node: true,
+		node: true
 	},
 	parserOptions: {
-		parser: 'babel-eslint',
+		parser: '@babel/eslint-parser',
+		requireConfigFile: false
 	},
 	extends: [
 		'@nuxtjs',
-		'prettier',
-		'prettier/vue',
-		'plugin:prettier/recommended',
-		'plugin:nuxt/recommended',
+		'plugin:nuxt/recommended'
 	],
-	plugins: ['prettier'],
 	// add your custom rules here
-	rules: {},
+	rules: {
+		indent: ['error', 'tab'],
+		'vue/html-indent': ['error', 'tab'],
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'comma-dangle': ['error', 'always-multiline'],
+		'space-before-function-paren': ['error', 'never'],
+		'vue/html-self-closing': [
+			'error',
+			{
+				html: {
+					void: 'always',
+					normal: 'always',
+					component: 'always'
+				},
+				svg: 'always',
+				math: 'always'
+			}
+		],
+		'vue/no-v-html': 0
+	}
 }
