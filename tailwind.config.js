d5a09c980fbbacef104fc81637809bca2326786e
HamidNE <hamidne@mail.ru>
2021-03-29 00:34:34 +0430

    feat: add tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	purge: {
+		content: [
+			'components/**/*.vue',
+			'layouts/**/*.vue',
+			'pages/**/*.vue',
+			'plugins/**/*.js',
+			'nuxt.config.js',
+		],
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
